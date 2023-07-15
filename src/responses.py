4febd3a49900d7fa4f558c207a0f6f8d6e794ae3
Logan Milligan <ljm92@uakron.edu>
2023-07-15 08:21:15 -0400

    I realized that i don't want to use python

--- src/responses.py
+++
@@ -1,5 +0,0 @@
-def handle_response(message) -> str:
-    p_message = message.lower()
-
-    if p_message == 'penis':
-        return 'waltuh put your dick away waltuh im not havin sex with you right now waltuh'
