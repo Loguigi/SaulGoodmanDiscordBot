af6e603e256cea8a7b587c7aac4dc8ca7603f2f0
Logan Milligan <123018204+Loguigi@users.noreply.github.com>
2023-07-15 08:07:38 -0400

    Added starter bot files

---
+++ src/responses.py
@@ -0,0 +1,5 @@
+def handle_response(message) -> str:
+    p_message = message.lower()
+
+    if p_message == 'penis':
+        return 'waltuh put your dick away waltuh im not havin sex with you right now waltuh'
