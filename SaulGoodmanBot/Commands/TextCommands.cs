aebb7277d4fda26042f986bd8b9cdc8f17262110
Logan Milligan <ljm92@uakron.edu>
2023-07-16 21:33:22 -0400

    Add coin flip command

---
+++ SaulGoodmanBot/Commands/TextCommands.cs
@@ -0,0 +1,29 @@
+/*
+    TextCommands.cs
+
+    Text commands for the bot
+
+    *Commands:
+    -flip: flip a coin
+*/
+
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+
+namespace SaulGoodmanBot.Commands;
+
+public class TextCommands : BaseCommandModule {
+    [Command("flip")]
+    public async Task CoinFlip(CommandContext cmd) {
+        var coin = new Random();
+        int flip = coin.Next(1, 3);
+        var response = new DiscordEmbedBuilder() {
+            Title = (flip == 1) ? "HEADS" : "TAILS",
+            Description = "Coin Flip Results",
+            Color = (flip == 1) ? DiscordColor.Aquamarine : DiscordColor.Rose
+        };
+
+        await cmd.RespondAsync(response);
+    }   
+}
