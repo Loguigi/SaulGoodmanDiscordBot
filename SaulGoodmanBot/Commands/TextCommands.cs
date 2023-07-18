dd1251112889a839b4a3a8f5a9e37980c57ecfe7
Logan Milligan <ljm92@uakron.edu>
2023-07-18 07:32:50 -0400

    Added 8 ball responses and added nuget package for slash commands

--- SaulGoodmanBot/Commands/TextCommands.cs
+++ SaulGoodmanBot/Commands/TextCommands.cs
@@ -10,14 +10,14 @@
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
-using DSharpPlus.CommandsNext;
-using DSharpPlus.CommandsNext.Attributes;
+using SaulGoodmanBot.External;
+
 
 namespace SaulGoodmanBot.Commands;
 
 public class TextCommands : BaseCommandModule {
     [Command("flip")]
-    public async Task CoinFlip(CommandContext cmd) {
+    public async Task CoinFlipCommand(CommandContext cmd) {
         var coin = new Random();
         int flip = coin.Next(1, 3);
         var response = new DiscordEmbedBuilder() {
@@ -30,10 +30,16 @@ public class TextCommands : BaseCommandModule {
     }
 
     [Command("8ball")]
-    public async Task Magic8Ball(CommandContext cmd, string question="") {
+    public async Task Magic8BallCommand(CommandContext cmd, string question="") {
         if (question == "") await cmd.RespondAsync("You must provide a question");
         else {
-            
+            var answer = new DiscordEmbedBuilder() {
+                Title = $"{cmd.Member.DisplayName} asked '{question}'",
+                Description = $"Answer: {Magic8Ball.GetAnswer()}",
+                Color = DiscordColor.Azure,
+            };
+
+            await cmd.RespondAsync(answer);
         }
     }
 }
