6f415ec511d076fa94e4e461d4b9fabe361ece67
Logan Milligan <ljm92@uakron.edu>
2023-07-24 06:10:23 -0400

    Restructure wheel picker class and commands

--- DataLibrary/Logic/WheelPickerProcessor.cs
+++ DataLibrary/Logic/WheelPickerProcessor.cs
@@ -9,11 +9,6 @@ public static class WheelPickerProcessor {
         return SqlDataAccess.SaveData<WheelPickerModel>(sql, new WheelPickerModel());
     }
 
-    public static List<WheelPickerModel> LoadWheel(ulong guildid, string name) {
-        string sql = @$"select WheelName, WheelOption, ImageUrl from dbo.Wheels where GuildId={guildid} and WheelName='{name}';";
-        return SqlDataAccess.LoadData<WheelPickerModel>(sql);
-    }
-
     public static List<WheelPickerModel> LoadAllWheels(ulong guildid) {
         string sql = @$"select WheelName, WheelOption from dbo.Wheels where GuildId={guildid};";
         return SqlDataAccess.LoadData<WheelPickerModel>(sql);
@@ -21,6 +16,11 @@ public static class WheelPickerProcessor {
 
     public static int DeleteWheelOption(ulong guildid, string name, string option) {
         string sql = @$"delete from dbo.Wheels where GuildId={guildid} and WheelName='{name}' and WheelOption='{option}';";
+        return SqlDataAccess.SaveData<WheelPickerModel>(sql, new WheelPickerModel());
+    }
+
+    public static int DeleteWheel(ulong guildid, string name) {
+        string sql = @$"delete from dbo.Wheels where GuildId={guildid} and WheelName='{name}';";
         return SqlDataAccess.SaveData<WheelPickerModel>(sql, new WheelPickerModel());
     }
 }
