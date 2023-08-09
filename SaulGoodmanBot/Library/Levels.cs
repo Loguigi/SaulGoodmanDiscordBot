f17f8cdb342b71f784309b08c154b3288d9c4cda
Logan Milligan <ljm92@uakron.edu>
2023-08-09 17:42:31 -0400

    Added level handler

--- SaulGoodmanBot/Library/Levels.cs
+++ SaulGoodmanBot/Library/Levels.cs
@@ -18,9 +18,22 @@ public class Levels {
         }
     }
 
+    public void GrantExp() {
+        Experience += EXP_GAIN;
+        int newLevel = (int)Math.Sqrt((Experience/2) + 1);
+        if (newLevel > Level) {
+            Level++;
+            LevelledUp = true;
+        }
+
+        LevelProcessor.UpdateExp(Guild.Id, User.Id, Level, Experience, MsgLastSent);
+    }
+
     private DiscordGuild Guild { get; set; }
     private DiscordUser User { get; set; }
     public int Level { get; set; }
     public int Experience { get; set; }
     public DateTime MsgLastSent { get; set; }
+    public bool LevelledUp { get; set; } = false;
+    private const int EXP_GAIN = 1;
 }
