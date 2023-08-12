b60dc435d88edd0110c40bc9e3371e2ea520a736
Logan Milligan <ljm92@uakron.edu>
2023-08-12 16:52:00 -0400

    Added custom birthday and level up messages

--- DataLibrary/Models/ConfigModel.cs
+++ DataLibrary/Models/ConfigModel.cs
@@ -7,8 +7,10 @@ public class ConfigModel {
     public long DefaultChannel { get; set; }
     public int BirthdayNotifications { get; set; } = 1;
     public DateTime PauseBdayNotifsTimer { get; set; }
+    public string BirthdayMessage { get; set; } = "Happy Birthday!";
     public string? ServerRolesName { get; set; } = null;
     public string? ServerRolesDescription { get; set; } = null;
     public int AllowMultipleRoles { get; set; } = 0;
     public int EnableLevels { get; set; } = 0;
+    public string LevelUpMessage { get; set; } = "has levelled up!";
 }
--- SaulGoodmanBot/Commands/ServerConfigCommands.cs
+++ SaulGoodmanBot/Commands/ServerConfigCommands.cs
@@ -11,69 +11,125 @@ namespace SaulGoodmanBot.Commands;
 [SlashCommandPermissions(Permissions.Administrator)]
 [SlashCommandGroup("config", "Bot configuration commands")]
 public class ServerConfigCommands : ApplicationCommandModule {
-    [SlashCommand("general", "General options like welcome and leave message")]
+    [SlashCommand("messags", "Bot message responses to events")]
     public async Task GeneralConfig(InteractionContext ctx,
         [Choice("Welcome message", "welcome")]
         [Choice("Leave message", "leave")]
+        [Choice("Birthday message", "birthday")]
+        [Choice("Level up message", "levelup")]
         [Option("option", "General config option")] string option) {
             
         var config = new ServerConfig(ctx.Guild);
         var intr = ctx.Client.GetInteractivity();
-        var description = string.Empty;
 
+        // Welcome Message
         if (option == "welcome") {
-            description = "Enter a welcome message for your server\nFormat is `[message] @user`\nEnter `cancel` to cancel or `disable` to disable welcome messages";
-            var prompt = new DiscordMessageBuilder()
-                .AddEmbed(new DiscordEmbedBuilder()
-                    .WithTitle("Set Welcome Message")
-                    .WithDescription(description));
+            var prompt = new DiscordEmbedBuilder()
+                .WithTitle("Set Welcome Message")
+                .WithDescription("Enter a welcome message for your server\nFormat is `[message] @user`\nEnter `cancel` to cancel or `disable` to disable welcome messages");
             
-            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder(prompt));
+            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder(new DiscordMessageBuilder().AddEmbed(prompt)));
 
             var response = await intr.WaitForMessageAsync(u => u.Author == ctx.Member, TimeSpan.FromSeconds(60));
-            if (response.Result.Content.ToLower().Contains("cancel")) {
+            if (response.Result.Content.ToLower().Contains("cancel") || response.TimedOut) {
                 // cancel operation
-                description = "Operation cancelled";
+                prompt.Description = "Operation cancelled";
             } else if (response.Result.Content.ToLower().Contains("disable")) {
                 // disable welcome message
-                description = "Welcome message disabled";
+                prompt.Description = "Welcome message disabled";
                 config.WelcomeMessage = null;
-                config.UpdateConfig();
             } else {
                 // save new welcome message
-                description = "Welcome message set";
+                prompt.Description = "Welcome message set";
                 config.WelcomeMessage = response.Result.Content;
-                config.UpdateConfig();
             }
 
-            await ctx.EditResponseAsync(new DiscordWebhookBuilder().AddEmbed(new DiscordEmbedBuilder().WithTitle("Set Welcome Message").WithDescription(description)));
+            config.UpdateConfig();
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().AddEmbed(prompt));
             await ctx.Channel.DeleteMessageAsync(response.Result);
+        
+        // Leave Message
         } else if (option == "leave") {
-            description = "Enter a leave message for your server\nFormat is `@user [message]`\nEnter `cancel` to cancel or `disable` to disable leave messages";
-            var prompt = new DiscordMessageBuilder()
-                .AddEmbed(new DiscordEmbedBuilder()
-                    .WithTitle("Set Leave Message")
-                    .WithDescription(description));
+            var prompt = new DiscordEmbedBuilder()
+                .WithTitle("Set Leave Message")
+                .WithDescription("Enter a leave message for your server\nFormat is `@user [message]`\nEnter `cancel` to cancel or `disable` to disable leave messages");
             
-            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder(prompt));
+            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder(new DiscordMessageBuilder().AddEmbed(prompt)));
 
             var response = await intr.WaitForMessageAsync(u => u.Author == ctx.Member, TimeSpan.FromSeconds(60));
-            if (response.Result.Content.ToLower().Contains("cancel")) {
+            if (response.Result.Content.ToLower().Contains("cancel") || response.TimedOut) {
                 // cancel operation
-                description = "Operation cancelled";
+                prompt.Description = "Operation cancelled";
             } else if (response.Result.Content.ToLower().Contains("disable")) {
                 // disable leave message
-                description = "Leave message disabled";
+                prompt.Description = "Leave message disabled";
                 config.LeaveMessage = null;
-                config.UpdateConfig();
             } else {
                 // save new leave message
-                description = "Leave message set";
+                prompt.Description = "Leave message set";
                 config.LeaveMessage = response.Result.Content;
-                config.UpdateConfig();
             }
 
-            await ctx.EditResponseAsync(new DiscordWebhookBuilder().AddEmbed(new DiscordEmbedBuilder().WithTitle("Set Leave Message").WithDescription(description)));
+            config.UpdateConfig();
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().AddEmbed(prompt));
+            await ctx.Channel.DeleteMessageAsync(response.Result);
+
+        // Birthday Message
+        } else if (option == "birthday") {
+            var prompt = new DiscordEmbedBuilder()
+                .WithTitle("Set Birthday Message")
+                .WithDescription("Enter a birthday message for your server\nFormat is `[message] @user`\nEnter `cancel` to cancel, `disable` to disable birthday messages, or `default` to reset the message");
+            
+            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder(new DiscordMessageBuilder().AddEmbed(prompt)));
+
+            var response = await intr.WaitForMessageAsync(u => u.Author == ctx.Member, TimeSpan.FromSeconds(60));
+            if (response.Result.Content.ToLower().Contains("cancel") || response.TimedOut) {
+                // cancel operation
+                prompt.Description = "Operation cancelled";
+            } else if (response.Result.Content.ToLower().Contains("disable")) {
+                // disable birthday message
+                prompt.Description = "Birthday messages disabled";
+                config.BirthdayNotifications = false;
+            } else if (response.Result.Content.ToLower().Contains("default")) {
+                // reset back to default
+                prompt.Description = "Birthday message set";
+                config.BirthdayNotifications = true;
+                config.BirthdayMessage = "Happy Birthday!";
+            } else {
+                // save new birthday message
+                prompt.Description = "Birthday message set";
+                config.BirthdayNotifications = true;
+                config.BirthdayMessage = response.Result.Content;
+            }
+
+            config.UpdateConfig();
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().AddEmbed(prompt));
+            await ctx.Channel.DeleteMessageAsync(response.Result);
+
+        // Level Up Message
+        } else if (option == "levelup") {
+            var prompt = new DiscordEmbedBuilder()
+                .WithTitle("Set Level Up Message")
+                .WithDescription("Enter a level up message for your server\nFormat is `@user [message]`\nEnter `cancel` to cancel or `default` to reset the message");
+            
+            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder(new DiscordMessageBuilder().AddEmbed(prompt)));
+
+            var response = await intr.WaitForMessageAsync(u => u.Author == ctx.Member, TimeSpan.FromSeconds(60));
+            if (response.Result.Content.ToLower().Contains("cancel") || response.TimedOut) {
+                // cancel operation
+                prompt.Description = "Operation cancelled";
+            } else if (response.Result.Content.ToLower().Contains("default")) {
+                // reset back to default
+                prompt.Description = "Level up message set";
+                config.LevelUpMessage = "has levelled up!";
+            } else {
+                // save new level up message
+                prompt.Description = "Level up message set";
+                config.LevelUpMessage = response.Result.Content;
+            }
+
+            config.UpdateConfig();
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().AddEmbed(prompt));
             await ctx.Channel.DeleteMessageAsync(response.Result);
         }
     }
