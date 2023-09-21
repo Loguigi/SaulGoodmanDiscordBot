321398208b14a020df1b30b3a7c404c42631fce2
Logan Milligan <ljm92@uakron.edu>
2023-09-21 08:41:25 -0400

    Add save schedule command (needs output)

--- SaulGoodmanBot/Commands/ScheduleCommands.cs
+++ SaulGoodmanBot/Commands/ScheduleCommands.cs
@@ -1,8 +1,40 @@
+using DSharpPlus.Entities;
 using DSharpPlus.SlashCommands;
 using SaulGoodmanBot.Library;
 
 namespace SaulGoodmanBot.Commands;
 
+[SlashCommandGroup("schedule", "Commands for saving users' work schedules to make hangout planning easier")]
+[GuildOnly]
 public class ScheduleCommands : ApplicationCommandModule {
-    
+    [SlashCommand("save", "Save a new schedule")]
+    public async Task SaveSchedule(InteractionContext ctx,
+        [Option("recurring", "False if your schedule changes every week, true if it does not change")] bool recurring,
+        [Option("sunday", "Schedule for Sunday")] string? sunday=null,
+        [Option("monday", "Schedule for Monday")] string? monday=null,
+        [Option("tuesday", "Schedule for Tuesday")] string? tuesday=null,
+        [Option("wednesday", "Schedule for Wednesday")] string? wednesday=null,
+        [Option("thursday", "Schedule for Thursday")] string? thursday=null,
+        [Option("friday", "Schedule for Friday")] string? friday=null,
+        [Option("saturday", "Schedule for Saturday")] string? saturday=null,
+        [Option("picture", "Picture of your schedule if you have one")] DiscordAttachment? picture=null) {
+
+        var schedule = new Schedule(ctx.Guild, ctx.User) {
+            LastUpdated = ctx.Interaction.CreationTimestamp.LocalDateTime,
+            RecurringSchedule = recurring,
+            WorkSchedule = {
+                {DayOfWeek.Sunday, sunday},
+                {DayOfWeek.Monday, monday},
+                {DayOfWeek.Tuesday, tuesday},
+                {DayOfWeek.Wednesday, wednesday},
+                {DayOfWeek.Thursday, thursday},
+                {DayOfWeek.Friday, friday},
+                {DayOfWeek.Saturday, saturday}
+            },
+            PictureUrl = picture?.Url
+        };
+        // schedule.Update();
+
+        
+    }
 }
--- SaulGoodmanBot/Library/Schedule.cs
+++ SaulGoodmanBot/Library/Schedule.cs
@@ -1,7 +1,48 @@
 using DSharpPlus.Entities;
+using DataLibrary.Logic;
+using DataLibrary.Models;
+using System.Data;
 
 namespace SaulGoodmanBot.Library;
 
 public class Schedule {
-    
+    public Schedule(DiscordGuild guild, DiscordUser user) {
+        Guild = guild;
+        User = user;
+        var data = ScheduleProcessor.ViewSchedule(Guild.Id, User.Id);
+
+        if (data == null) {
+            ScheduleProcessor.SaveSchedule(new ScheduleModel() { GuildId = (long)Guild.Id, UserId=(long)User.Id });
+            return;
+        }
+
+        for (var i = DayOfWeek.Sunday; i <= DayOfWeek.Saturday; ++i) {
+            WorkSchedule[i] = data.ToList()[(int)i];
+        }
+        LastUpdated = data.LastUpdated;
+        RecurringSchedule = data.RecurringSchedule == 1;
+        PictureUrl = data.PictureUrl;
+    }
+
+    public void Update() {
+        ScheduleProcessor.UpdateSchedule(new ScheduleModel() {
+            GuildId = (long)Guild.Id
+        });
+    }
+
+
+    private DiscordGuild Guild { get; set; }
+    public DiscordUser User { get; private set; }
+    public DateTime? LastUpdated { get; set; }
+    public bool RecurringSchedule { get; set; } = false;
+    public Dictionary<DayOfWeek, string?> WorkSchedule { get; set; } = new() {
+        {DayOfWeek.Sunday, null},
+        {DayOfWeek.Monday, null},
+        {DayOfWeek.Tuesday, null},
+        {DayOfWeek.Wednesday, null},
+        {DayOfWeek.Thursday, null},
+        {DayOfWeek.Friday, null},
+        {DayOfWeek.Saturday, null}
+    };
+    public string? PictureUrl { get; set; } = null;
 }
