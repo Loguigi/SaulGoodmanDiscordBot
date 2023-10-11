9e6debe4dd7f81e43edffc3bb9c7e97d00fe0135
Logan Milligan <ljm92@uakron.edu>
2023-10-11 09:55:19 -0400

    Refactored code and enhanced wheel spin, wheel list, and wheel add commands

--- SaulGoodmanBot/Library/IDHelper.cs
+++ SaulGoodmanBot/Library/IDHelper.cs
@@ -1,6 +1,12 @@
 namespace SaulGoodmanBot.Library;
 
 public static class IDHelper {
+    public static class WheelPicker {
+        public static readonly string Add = "ADDOPTIONS";
+        public static readonly string List = "LISTWHEELS";
+        public static readonly string Spin = "SPINWHEEL";
+        public static readonly string Delete = "DELETEWHEEL";
+    }
     public static class Schedules {
         public static readonly string Today = "TODAYSCHEDULE";
     }
--- SaulGoodmanBot/Schema/Wheels.sql
+++ SaulGoodmanBot/Schema/Wheels.sql
@@ -2,5 +2,6 @@ CREATE TABLE [dbo].[Wheels] (
     [GuildId]     BIGINT          NOT NULL,
     [WheelName]   NVARCHAR(100)   NOT NULL,
     [WheelOption] NVARCHAR(100)   NOT NULL,
-    [ImageUrl]    NVARCHAR(1000)  NULL
+    [ImageUrl]    NVARCHAR(1000)  NULL,
+    [TempRemoved] BIT             NOT NULL
 );
