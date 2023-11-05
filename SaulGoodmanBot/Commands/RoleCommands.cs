3fa266dc8c8ca7515a12cb560819df27d69f8992
Logan Milligan <ljm92@uakron.edu>
2023-11-05 14:22:32 -0500

    Not too sure what I've done

--- SaulGoodmanBot/Commands/RoleCommands.cs
+++ SaulGoodmanBot/Commands/RoleCommands.cs
@@ -38,41 +38,42 @@ public class RoleCommands : ApplicationCommandModule {
         var roles = new ServerRoles(ctx.Guild, ctx.Client);
 
         // ERROR: roles not setup
-        if (roles.IsNotSetup()) 
-            await ctx.CreateResponseAsync(StandardOutput.Error("Self-assignable roles not setup yet. Use /role setup"), ephemeral:true); 
-
-        // Add role
-        else {
-            // ERROR: role already added
-            if (roles.AlreadyExists(role))
-                await ctx.CreateResponseAsync(StandardOutput.Error($"{role.Mention} already added to {roles.CategoryName}"), ephemeral:true);
-            else if (emoji != null) {
-
-                // Standard unicode emoji
-                if (DiscordEmoji.TryFromUnicode(ctx.Client, emoji, out DiscordEmoji normalEmoji)) {
-                    roles.Add(new RoleComponent(role, description, normalEmoji));
-                    await ctx.CreateResponseAsync(StandardOutput.Success($"Added {role.Mention} to {roles.CategoryName}"), ephemeral:true);
-
-                // Not standard unicode emoji; possible guild emoji
-                } else if (emoji.Contains(':')) {
-                    var tryEmoji = ":" + string.Join("", emoji.SkipWhile(x => x != ':').Skip(1).TakeWhile(x => x != ':')) + ":";
-
-                    if (DiscordEmoji.TryFromName(ctx.Client, tryEmoji, true, out DiscordEmoji guildEmoji)) {
-                        roles.Add(new RoleComponent(role, description, guildEmoji));
-                        await ctx.CreateResponseAsync(StandardOutput.Success($"Added {role.Mention} to {roles.CategoryName}"), ephemeral:true);
-
-                    // ERROR: emoji error
-                    } else
-                        await ctx.CreateResponseAsync(StandardOutput.Error("Invalid emoji"), ephemeral:true);
-
-                // ERROR: emoji error
-                } else 
-                    await ctx.CreateResponseAsync(StandardOutput.Error("Invalid emoji"), ephemeral:true);
-            } else {
-                roles.Add(new RoleComponent(role, description, null));
-                await ctx.CreateResponseAsync(StandardOutput.Success($"Added {role.Mention} to {roles.CategoryName}"), ephemeral:true);
-            }
+        if (roles.IsNotSetup()) {
+            await ctx.CreateResponseAsync(StandardOutput.Error("Self-assignable roles not setup yet. Use /role setup"), ephemeral:true);
+            return;
         }
+
+        if (roles.AlreadyExists(role)) {
+            await ctx.CreateResponseAsync(StandardOutput.Error($"{role.Mention} already added to {roles.CategoryName}"), ephemeral:true);
+            return;
+        }
+
+        if (emoji == null) {
+            roles.Add(new RoleComponent(role, description, null));
+            await ctx.CreateResponseAsync(StandardOutput.Success($"Added {role.Mention} to {roles.CategoryName}"), ephemeral:true);
+            return;
+        }
+
+        // Standard unicode emoji
+        if (DiscordEmoji.TryFromUnicode(ctx.Client, emoji, out DiscordEmoji normalEmoji)) {
+            roles.Add(new RoleComponent(role, description, normalEmoji));
+            await ctx.CreateResponseAsync(StandardOutput.Success($"Added {role.Mention} to {roles.CategoryName}"), ephemeral:true);
+
+        // Not standard unicode emoji; possible guild emoji
+        } else if (emoji.Contains(':')) {
+            var tryEmoji = ":" + string.Join("", emoji.SkipWhile(x => x != ':').Skip(1).TakeWhile(x => x != ':')) + ":";
+
+            if (DiscordEmoji.TryFromName(ctx.Client, tryEmoji, true, out DiscordEmoji guildEmoji)) {
+                roles.Add(new RoleComponent(role, description, guildEmoji));
+                await ctx.CreateResponseAsync(StandardOutput.Success($"Added {role.Mention} to {roles.CategoryName}"), ephemeral:true);
+
+            // ERROR: emoji error
+            } else
+                await ctx.CreateResponseAsync(StandardOutput.Error("Invalid emoji"), ephemeral:true);
+
+        // ERROR: emoji error
+        } else 
+            await ctx.CreateResponseAsync(StandardOutput.Error("Invalid emoji"), ephemeral:true);
     }
 
     [SlashCommand("remove", "Remove a role from the list of self-assignable roles")]
@@ -80,120 +81,94 @@ public class RoleCommands : ApplicationCommandModule {
     public async Task RemoveRole(InteractionContext ctx) {
         var roles = new ServerRoles(ctx.Guild, ctx.Client);
 
-        // ERROR: roles not setup
-        if (roles.IsNotSetup())
+        if (roles.IsNotSetup()) {
             await ctx.CreateResponseAsync(StandardOutput.Error("Self-assignable roles not setup yet. Use /role setup"), ephemeral:true);
-        else {
-            // Create dropdown
-            var roleOptions = new List<DiscordSelectComponentOption>() {
-                new DiscordSelectComponentOption("Cancel", "cancel", "", false, new DiscordComponentEmoji(DiscordEmoji.FromName(ctx.Client, ":x:", false)))
-            };
-            foreach (var role in roles.Roles) {
-                roleOptions.Add(new DiscordSelectComponentOption(role.Role.Name, role.Role.Id.ToString(), "", false));
-            }
-            var roleDropdown = new DiscordSelectComponent("removeroledropdown", "Select a role", roleOptions);
-
-            // Send prompt
-            var prompt = new DiscordMessageBuilder()
-                .AddEmbed(new DiscordEmbedBuilder()
-                    .WithTitle("Remove Role")
-                    .WithColor(DiscordColor.DarkRed))
-                .AddComponents(roleDropdown);
-            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder(prompt));
-
-            // Add handler
-            ctx.Client.ComponentInteractionCreated += RoleHandler.HandleRemoveRole;
+            return;
         }
+
+        // Create dropdown
+        var roleOptions = new List<DiscordSelectComponentOption>();
+        roles.Roles.ForEach(r => roleOptions.Add(new DiscordSelectComponentOption(r.Role.Name, r.Role.Id.ToString(), "", false)));
+        var roleDropdown = new DiscordSelectComponent(IDHelper.Roles.REMOVE, "Select a role", roleOptions);
+        var cancelButton = new DiscordButtonComponent(ButtonStyle.Secondary, IDHelper.Roles.REMOVE, "Cancel", false, new DiscordComponentEmoji(DiscordEmoji.FromName(ctx.Client, ":arrow_left:, false")));
+
+        var prompt = new DiscordMessageBuilder()
+            .AddEmbed(new DiscordEmbedBuilder()
+                .WithTitle("Remove Role")
+                .WithColor(DiscordColor.DarkRed))
+            .AddComponents(roleDropdown);
+        await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder(prompt));
+
+        ctx.Client.ComponentInteractionCreated += RoleHandler.HandleRemoveRole;
     }
 
     [SlashCommand("menu", "Creates a menu for assigning and unassigning roles")]
     public async Task RoleMenu(InteractionContext ctx) {
         var roles = new ServerRoles(ctx.Guild, ctx.Client);
 
-        // ERROR: not setup
-        if (roles.IsNotSetup())
+        if (roles.IsNotSetup()) {
             await ctx.CreateResponseAsync(StandardOutput.Error("Self-assignable roles not setup yet. Use /role setup"), ephemeral:true);
-
-        // ERROR: no roles added
-        else if (roles.IsEmpty())
-            await ctx.CreateResponseAsync(StandardOutput.Error("No roles added. Use /role add"), ephemeral:true);
-
-        // Display menu
-        else {
-            // Create role options
-            var roleOptions = new List<DiscordSelectComponentOption>();
-            foreach (var role in roles.Roles) {
-                if (!role.Emoji.Equals(null)) {
-                    roleOptions.Add(new DiscordSelectComponentOption(role.Role.Name, role.Role.Id.ToString(), role.Description ?? string.Empty, false, new DiscordComponentEmoji(role.Emoji)));
-                } else {
-                    roleOptions.Add(new DiscordSelectComponentOption(role.Role.Name, role.Role.Id.ToString(), role.Description ?? string.Empty, false));
-                }
-            }
-
-            // Create dropdown
-            DiscordSelectComponent roleDropdown;
-            if (roles.AllowMultipleRoles)
-                roleDropdown = new("rolemenudropdown", "Select roles", roleOptions, false, 1, roleOptions.Count);
-            else
-                roleDropdown = new("rolemenudropdown", "Select a role", roleOptions, false);
-
-            // Send prompt
-            var prompt = new DiscordEmbedBuilder()
-                .WithAuthor(ctx.Guild.Name, "https://youtu.be/dQw4w9WgXcQ", ctx.Guild.IconUrl)
-                .WithTitle(roles.CategoryName)
-                .WithDescription(roles.CategoryDescription)
-                .WithFooter(roles.AllowMultipleRoles ? "Can have multiple" : "Can only have one")
-                .WithColor(DiscordColor.Turquoise);
-            foreach (var r in roles.Roles) {
-                if (r == roles.Roles.First())
-                    prompt.AddField("Available Roles", r.Role.Mention);
-                else
-                    prompt.Fields.Where(x => x.Name == "Available Roles").First().Value += $", {r.Role.Mention}";
-            }
-            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder(new DiscordMessageBuilder().AddEmbed(prompt).AddComponents(roleDropdown)));
-
-            // Add handler
-            ctx.Client.ComponentInteractionCreated -= RoleHandler.HandleMenu;
-            ctx.Client.ComponentInteractionCreated += RoleHandler.HandleMenu;
+            return;
         }
+
+        if (roles.IsEmpty()) {
+            await ctx.CreateResponseAsync(StandardOutput.Error("No roles added. Use /role add"), ephemeral:true);
+            return;
+        }
+        
+        var roleOptions = new List<DiscordSelectComponentOption>();
+        roles.Roles.ForEach(r => roleOptions.Add(new DiscordSelectComponentOption(r.Role.Name, r.Role.Id.ToString(), r.Description ?? string.Empty, false, new DiscordComponentEmoji(r.Emoji ?? DiscordEmoji.FromName(ctx.Client, ":arrow_right:", false)))));
+        DiscordSelectComponent roleDropdown = roles.AllowMultipleRoles ? new(IDHelper.Roles.MENU, "Select roles", roleOptions, false, 1, roleOptions.Count) : new(IDHelper.Roles.MENU, "Select a role", roleOptions, false);
+
+        var embed = new DiscordEmbedBuilder()
+            .WithAuthor(ctx.Guild.Name, "https://youtu.be/dQw4w9WgXcQ", ctx.Guild.IconUrl)
+            .WithTitle(roles.CategoryName)
+            .WithDescription(roles.CategoryDescription)
+            .WithFooter(roles.AllowMultipleRoles ? "Can have multiple" : "Can only have one")
+            .WithColor(DiscordColor.Turquoise);
+
+        foreach (var r in roles.Roles) {
+            if (r == roles.Roles.First())
+                embed.AddField("Available Roles", r.Role.Mention);
+            else
+                embed.Fields.Where(x => x.Name == "Available Roles").First().Value += $", {r.Role.Mention}";
+        }
+        await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder(new DiscordMessageBuilder().AddEmbed(embed).AddComponents(roleDropdown)));
+
+        // Add handler
+        ctx.Client.ComponentInteractionCreated -= RoleHandler.HandleMenu;
+        ctx.Client.ComponentInteractionCreated += RoleHandler.HandleMenu;
     }
 
     [SlashCommand("assign", "Assign or unassign a role")]
     public async Task AssignRole(InteractionContext ctx) {
         var roles = new ServerRoles(ctx.Guild, ctx.Client);
 
-        // ERROR: not setup
-        if (roles.IsNotSetup()) 
+        if (roles.IsNotSetup()) {
             await ctx.CreateResponseAsync(StandardOutput.Error("Self-assignable roles not setup yet. Use /role setup"), ephemeral:true);
-
-        // ERROR: no roles added
-        else if (roles.IsEmpty())
-            await ctx.CreateResponseAsync(StandardOutput.Error("No roles added. Use /role add"), ephemeral:true);
-
-        else {
-            // Create role options and dropdowns
-            var roleOptions = new List<DiscordSelectComponentOption>() {new DiscordSelectComponentOption("Cancel", "cancel", "", false, new DiscordComponentEmoji(DiscordEmoji.FromName(ctx.Client, ":x:")))};
-            foreach (var role in roles.Roles) {
-                if (role.Emoji != null) {
-                    roleOptions.Add(new DiscordSelectComponentOption(role.Role.Name, role.Role.Id.ToString(), role.Description ?? string.Empty, false, new DiscordComponentEmoji(role.Emoji)));
-                } else {
-                    roleOptions.Add(new DiscordSelectComponentOption(role.Role.Name, role.Role.Id.ToString(), role.Description ?? string.Empty, false));
-                }
-            }
-            var roleDropdown = new DiscordSelectComponent("roleassigndropdown", "Select a role", roleOptions);
-        
-            // Display prompt
-            var prompt = new DiscordMessageBuilder()
-                .AddEmbed(new DiscordEmbedBuilder()
-                    .WithTitle(roles.CategoryName)
-                    .WithDescription(roles.CategoryDescription)
-                    .WithColor(DiscordColor.Turquoise))
-                .AddComponents(roleDropdown);
-            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder(prompt));
-
-            // Add handler
-            ctx.Client.ComponentInteractionCreated += RoleHandler.HandleAssign;
+            return;
         }
+
+        if (roles.IsEmpty()) {
+            await ctx.CreateResponseAsync(StandardOutput.Error("No roles added. Use /role add"), ephemeral:true);
+            return;
+        }
+
+        // Create role options and dropdowns
+        var roleOptions = new List<DiscordSelectComponentOption>();
+        roles.Roles.ForEach(r => roleOptions.Add(new DiscordSelectComponentOption(r.Role.Name, r.Role.Id.ToString(), r.Description ?? string.Empty, false, new DiscordComponentEmoji(r.Emoji ?? DiscordEmoji.FromName(ctx.Client, ":arrow_right:", false)))));
+        var roleDropdown = new DiscordSelectComponent(IDHelper.Roles.ASSIGN, "Select a role", roleOptions);
+        var cancelButton = new DiscordButtonComponent(ButtonStyle.Secondary, IDHelper.Roles.ASSIGN, "Cancel", false, new DiscordComponentEmoji(DiscordEmoji.FromName(ctx.Client, ":arrow_left:, false")));
+        
+        var embed = new DiscordEmbedBuilder()
+            .WithTitle(roles.CategoryName)
+            .WithDescription(roles.CategoryDescription)
+            .WithColor(DiscordColor.Turquoise);
+
+        await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder(new DiscordMessageBuilder().AddEmbed(embed).AddComponents(roleDropdown).AddComponents(cancelButton)));
+
+        ctx.Client.ComponentInteractionCreated -= RoleHandler.HandleAssign;
+        ctx.Client.ComponentInteractionCreated += RoleHandler.HandleAssign;
 
     }
 }
