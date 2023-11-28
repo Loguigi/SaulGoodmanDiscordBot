e282b26551c580aa5b0898f60a60e5f709c152a1
Logan Milligan <ljm92@uakron.edu>
2023-11-27 19:45:33 -0500

    Add address to gift exchange location

--- DataLibrary/Models/SantaConfigModel.cs
+++ DataLibrary/Models/SantaConfigModel.cs
@@ -5,6 +5,7 @@ public class SantaConfigModel {
     public DateTime ParticipationDeadline { get; set; }
     public DateTime ExchangeDate { get; set; }
     public string ExchangeLocation { get; set; } = string.Empty;
+    public string? ExchangeAddress { get; set; } = null;
     public double? PriceLimit { get; set; } = null;
     public int LockedIn { get; set; } = 0;
 }
