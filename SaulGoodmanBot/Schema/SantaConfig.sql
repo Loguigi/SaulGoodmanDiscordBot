53984b3a19c6b4a77253f277238d99b625cd4680
Logan Milligan <ljm92@uakron.edu>
2023-12-06 00:01:11 -0500

    Fix @everyone mentions and add Secret Santa role

--- SaulGoodmanBot/Schema/SantaConfig.sql
+++ SaulGoodmanBot/Schema/SantaConfig.sql
@@ -1,8 +1,9 @@
 CREATE TABLE [dbo].[SantaConfig] (
     [GuildId] BIGINT NOT NULL,
+    [SantaRoleId] BIGINT NOT NULL,
     [ParticipationDeadline] DATETIME NOT NULL,
     [ExchangeDate] DATETIME NOT NULL,
-    [ExchangeLocation] NVARCHAR(30) NOT NULL,
+    [ExchangeLocation] NVARCHAR(100) NOT NULL,
     [ExchangeAddress] NVARCHAR(100) NULL,
     [PriceLimit] DECIMAL NULL,
     [LockedIn] BIT NOT NULL,
